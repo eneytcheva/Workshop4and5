d2231c1a5a5724cec846c6e40f16e5f0e824082d
Elitza Neytcheva <eneytcheva@gmail.com>
2016-11-10 20:39:29 -0500

    fb7

--- app/components/comment.js
+++ app/components/comment.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import {unixTimeToString} from '../util';
+import {Link} from 'react-router';
 
 export default class Comment extends React.Component {
     render() {
         return (
-          <div>
-            <div className="media-left media-top">
-            PIC
+            <div>
+                <div className="media-left media-top">
+                    PIC
+                </div>
+                <div className="media-body">
+                    <Link to={"/profile/" + this.props.author._id}>
+                        {this.props.author.fullName}
+                    </Link>
+                    {this.props.children}
+                    <br/>
+                    <a href="#">Like</a>
+                    ·
+                    <a href="#">Reply</a>
+                    · {unixTimeToString(this.props.postDate)}
+                </div>
             </div>
-            <div className="media-body">
-              <a href="#">{this.props.author.fullName}</a> {this.props.children}
-              <br /><a href="#">Like</a> · <a href="#">Reply</a> ·
-                {unixTimeToString(this.props.postDate)}
-            </div>
-          </div>
         )
     }
 }
